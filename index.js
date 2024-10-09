84e9032674f1da0e382fcc2c84d2fb06d807fc52
Taran66 <taranpreet1911@gmail.com>
2024-10-09 14:50:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import getPosts, { getPostsLength } from "./postController.js";
+
+console.log(getPosts())
+
+console.log(`Post Length ${getPostsLength()}`)
+
+// const generateRandomNumber = require('./utils')
+
+// console.log(`Random Number: ${generateRandomNumber()}`)
